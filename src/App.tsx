1421908289b668633c77d93d33fdbcd1d095b620
Yanick <yanick.burgermeister@gmail.com>
2023-11-21 16:04:37 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import  BASKETBALLCOURT from './images/basketballCourt.png'
+
+function App() {
+
+  return (
+    <div style={{backgroundImage: BASKETBALLCOURT}}>
+      <h1>Hallo</h1>
+    </div>
+  )
+}
+
+export default App
